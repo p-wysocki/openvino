4ed08c8cf7168e3e8dab1e7b939528a54a6bbaa8
Piotr Kowalczyk <piotr.kowalczyk@intel.com>
2026-07-28 13:50:19 +0000

    [taskSystem]: Added dummy test.

---
+++ MEGAKERNEL_POC/research/preloading_gemv/src/taskSystemTests.cpp
@@ -0,0 +1,10 @@
+#include "../../common/oclTestFixture.h"
+
+
+class TaskSystemTests : public ocltest::OclTestFixture {};
+
+
+TEST_F(TaskSystemTests, DummyTest) {
+  // This test is a placeholder for future task system tests.
+  ASSERT_TRUE(true);
+}
