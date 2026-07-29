846dfc72cad853bab118006c49d6c63c3cc73627
Piotr Kowalczyk <piotr.kowalczyk@intel.com>
2026-07-29 13:00:46 +0000

    [taskSystem]: WIP

--- MEGAKERNEL_POC/research/preloading_gemv/src/taskSystemTests.cpp
+++ MEGAKERNEL_POC/research/preloading_gemv/src/taskSystemTests.cpp
@@ -84,11 +84,6 @@ TEST_F(TaskSystemTests, ClaimsOneHundredTasks) {
       TASK_SYSTEM_KERNEL_PATH + "taskManagerTest.cl", "taskManagerTest",
       "-I " + TASK_SYSTEM_KERNEL_PATH);
 
-  cl_int status = CL_SUCCESS;
-  cl_mem claimedTaskIds = clCreateBuffer(
-      context(), CL_MEM_READ_WRITE, taskCount * sizeof(int), nullptr, &status);
-  ASSERT_OCL_SUCCESS(status);
-
   std::vector<TaskDesc> hostTasks(taskCount);
   for (size_t index = 0; index < taskCount; ++index) {
     hostTasks[index].input = nullptr;
@@ -98,42 +93,47 @@ TEST_F(TaskSystemTests, ClaimsOneHundredTasks) {
     hostTasks[index].taskType = GEMV;
   }
 
+  cl_int status = CL_SUCCESS;
   TaskManager taskManager;
   ASSERT_OCL_SUCCESS(HostInitalizeTaskSystem(taskManager, hostTasks, deviceId(),
                                              context(), queue()));
-
-  std::vector<int> actualTaskIds(taskCount, -1);
-  ASSERT_OCL_SUCCESS(clEnqueueWriteBuffer(
-      queue(), claimedTaskIds, CL_TRUE, 0, taskCount * sizeof(int),
-      actualTaskIds.data(), 0, nullptr, nullptr));
-
   cl_mem taskManagerBuffer =
       clCreateBuffer(context(), CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
                      sizeof(taskManager), &taskManager, &status);
   ASSERT_OCL_SUCCESS(status);
 
+  std::vector<int> taskExecutedHost(taskCount, -1);
+  cl_mem taskExecutedGPU = clCreateBuffer(
+      context(), CL_MEM_READ_WRITE, taskCount * sizeof(int), nullptr, &status);
+  ASSERT_OCL_SUCCESS(status);
+
+  ASSERT_OCL_SUCCESS(clEnqueueWriteBuffer(
+      queue(), taskExecutedGPU, CL_TRUE, 0, taskCount * sizeof(int),
+      taskExecutedHost.data(), 0, nullptr, nullptr));
+
   ASSERT_OCL_SUCCESS(
       clSetKernelArg(binary.kernel, 0, sizeof(cl_mem), &taskManagerBuffer));
   ASSERT_OCL_SUCCESS(
-      clSetKernelArg(binary.kernel, 1, sizeof(cl_mem), &claimedTaskIds));
+      clSetKernelArg(binary.kernel, 1, sizeof(cl_mem), &taskExecutedGPU));
 
   const size_t globalWorkSize = WORKERS * THREADS;
   ASSERT_OCL_SUCCESS(clEnqueueNDRangeKernel(queue(), binary.kernel, 1, nullptr,
                                             &globalWorkSize, &THREADS, 0,
                                             nullptr, nullptr));
+
   ASSERT_OCL_SUCCESS(clEnqueueReadBuffer(
-      queue(), claimedTaskIds, CL_TRUE, 0, taskCount * sizeof(int),
-      actualTaskIds.data(), 0, nullptr, nullptr));
+      queue(), taskExecutedGPU, CL_TRUE, 0, taskCount * sizeof(int),
+      taskExecutedHost.data(), 0, nullptr, nullptr));
 
   ASSERT_OCL_SUCCESS(clReleaseMemObject(taskManagerBuffer));
-  ASSERT_OCL_SUCCESS(clReleaseMemObject(claimedTaskIds));
+  ASSERT_OCL_SUCCESS(clReleaseMemObject(taskExecutedGPU));
   ASSERT_OCL_SUCCESS(HostReleaseTaskSystem(taskManager, deviceId(), context()));
   releaseOCLBinary(binary);
 
-  for (int i = 0; i < actualTaskIds.size(); ++i) {
+  for (int i = 0; i < taskExecutedHost.size(); ++i) {
     std::cout << "Task with id <" << i << "> was executed by "
-              << actualTaskIds[i] << std::endl;
-    ASSERT_GE(actualTaskIds[i], 0);
+              << taskExecutedHost[i] << std::endl;
+    ASSERT_GE(taskExecutedHost[i], 0);
   }
 }
 
